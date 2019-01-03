402a70085d1e8012edbcdf9b2dc2d626072d63b1
libin.zhou <binteam2018@163.com>
2019-01-03 09:21:35 +0800

    dev-core-basic conf success

---
+++ src/renderer/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <index-page/>
+</template>
+
+<script>
+    import IndexPage from './version-1.0/IndexPage/IndexPage'
+    export default {
+      name: 'index',
+      components: {IndexPage}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/renderer/views/version-1.0/IndexPage/IndexPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <button @click="incrment_main_counter">+</button>
+        <button @click="decrement_main_counter">-</button>
+        <div>{{Counter}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'IndexPage',
+      data () {
+        return {
+
+        }
+      },
+      computed: {
+        Counter () {
+          // 计数器
+          return this.$store.state.Counter.main
+        }
+      },
+      created () {
+
+      },
+      methods: {
+        decrement_main_counter () {
+          this.$store.dispatch('DECREMENT_MAIN_COUNTER')
+        },
+        incrment_main_counter () {
+          this.$store.dispatch('INCREMENT_MAIN_COUNTER')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
